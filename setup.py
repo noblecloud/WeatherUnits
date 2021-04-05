9310ac10f7fca1966d79e0423da945335e537634
noblecloud <git@noblecloud.io>
2021-04-05 10:43:46 -0400

    Incremented x.x.y version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 	name="WeatherUnits",
-	version="0.1.2",
+	version="0.1.3",
 	license='MIT license',
 	author="noblecloud",
 	author_email="git@noblecloud.io",
