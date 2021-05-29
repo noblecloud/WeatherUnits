fd24b3767e2d398e8da885f181463a0b4f8f6a85
Bryan Neal Garrison <git@noblecloud.io>
2021-05-29 15:06:16 -0400

    Instrument version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 		name="WeatherUnits",
-		version="0.2.2",
+		version="0.2.3",
 		license='MIT license',
 		author="noblecloud",
 		author_email="git@noblecloud.io",
