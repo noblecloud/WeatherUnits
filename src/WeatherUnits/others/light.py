a6f4a7786073a550dcd38168979847787011f0ff
Bryan Neal Garrison <git@noblecloud.io>
2021-06-17 22:05:05 -0400

    Removed formatting now that is is default for all Measurement

--- src/WeatherUnits/others/light.py
+++ src/WeatherUnits/others/light.py
@@ -17,19 +17,9 @@ class Irradiance(Light):
 	_unit = 'W/m²'
 
 
-@MeasurementGroup
+@NamedType
 class Illuminance(Light):
 	_unit = 'lux'
-
-	def __new__(cls, value):
-		if value > 1000:
-			cls._format = '{2:1f}'
-			cls._decorator = 'k'
-			value /= 1000
-		return float.__new__(cls, value)
-
-	def __init__(self, value):
-		float.__init__(value)
 
 
 RadiantFlux = Irradiance
