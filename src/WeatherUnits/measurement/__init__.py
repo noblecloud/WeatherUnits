09a1ca5aac3098d16131dd559ddd31b33ff32166
Bryan Neal Garrison <git@noblecloud.io>
2021-06-18 15:55:15 -0400

    Added showUnit option and size sizeHint

--- src/WeatherUnits/measurement/__init__.py
+++ src/WeatherUnits/measurement/__init__.py
@@ -40,13 +40,13 @@ class SmartFloat(float):
 	_config = _config
 	_precision: int = 1
 	_max: int = 3
-	_lengthHint: int = None
 	_unit: str = ''
 	_suffix: str = ''
 	_decorator: str = ''
 	_unitSpacer = True
 	_title: str = ''
 	_scale = 1
+	_showUnit: bool = True
 	_shorten: bool = True
 	_thousandsSeparator = False
 
@@ -60,32 +60,34 @@ class SmartFloat(float):
 			self._title = value._title
 		float.__init__(value)
 
-	def __str__(self) -> str:
+	def _string(self, hintString: bool = False, forceUnit: bool = False) -> str:
 		if self._shorten:
-			c, valueString = 0, float(self)
-			while 4 <= len(str(int(valueString))) > self._max:
+			c, valueFloat = 0, float(self)
+			while 4 <= len(str(int(valueFloat))) > self._max:
 				c += 1
-				valueString /= 1000
+				valueFloat /= 1000
 			suffix = ['', 'k', 'm', 'B'][c]
 		else:
-			valueString = self
+			valueFloat = self
 			c = False
 			suffix = ''
 
-		newScale, decimal = list(len(n) for n in str(round(valueString, self._precision)).strip('0').split('.'))
+		newScale, decimal = list(len(n) for n in str(round(valueFloat, self._precision)).strip('0').split('.'))
 		decimal = min(self._precision, 1 if not decimal and c else decimal)
-		self._lengthHint = newScale + decimal
 		formatStr = f"{{:{',' if self._thousandsSeparator else ''}.{decimal}f}}"
-		valueString = formatStr.format(valueString)
+		valueString = formatStr.format(10 ** newScale if hintString else valueFloat)
+		unit = '' if not self._showUnit or forceUnit else f'{" " if self._unitSpacer else ""}{self._unit}'
+		return f'{valueString}{suffix}{self._decorator}{unit}'
 
-		return f'{valueString}{suffix}{self._decorator}'
+	def __str__(self):
+		return self._string()
 
 	def __bool__(self):
 		return super().__bool__() or bool(self.unit)
 
 	@property
 	def withUnit(self):
-		return f'{str(self)}{" " if self._unitSpacer else ""}{self._unit}'
+		return self._string(forceUnit=True)
 
 	@property
 	def unit(self) -> str:
@@ -114,6 +116,19 @@ class SmartFloat(float):
 	@title.setter
 	def title(self, value):
 		self._title = value
+
+	@property
+	def showUnit(self):
+		return self._showUnit
+
+	@showUnit.setter
+	def showUnit(self, value):
+		self._showUnit = value
+
+	@property
+	def sizeHint(self) -> int:
+		# length = len(string) - (0.5 * (string.count('.') + string.count(','))
+		return self._string(hintString=True)
 
 
 class Measurement(SmartFloat):
@@ -236,12 +251,9 @@ class MeasurementSystem(Measurement):
 
 		# If values are siblings change to sibling class with changeScale()
 		if siblings:
+			# If both are variants convert value to baseUnit first
 			value = value._baseUnit(value) if variant else value
 			return cls(value.__class__.changeScale(value, cls._Scale[cls.__name__]))
-
-		# If values are siblings and both variants
-			# intermediate = cls(value.__class__.changeScale(value, cls._Scale.Base))
-			# return cls(value.__class__.changeScale(value, intermediate._Scale[intermediate.__class__.__name__]))
 
 		# If values are cousins initiate with values base sibling causing a recursive call to __new__
 		elif cousins:
