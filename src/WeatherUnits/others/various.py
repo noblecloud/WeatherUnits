4cc1c5734828a8ff0bf9aeacd792ed1fd6213a24
Bryan Neal Garrison <git@noblecloud.io>
2021-06-20 16:32:06 -0400

    Fixed string production for direction

--- src/WeatherUnits/others/various.py
+++ src/WeatherUnits/others/various.py
@@ -1,5 +1,32 @@
 from .. import Measurement, NamedType, PropertiesFromConfig
 
+@NamedType
+@PropertiesFromConfig
+class Direction(Measurement):
+	_precision = 0
+	_decorator = 'º'
+	_cardinal = True
+	_degrees = False
+
+	def __init__(self, *args, **kwargs):
+		super(Direction, self).__init__(*args, **kwargs)
+
+	def __str__(self) -> str:
+		string = string = self._string()
+		if self._cardinal and self._degrees:
+			return f'{self.cardinal} ({self._string()})'
+		if self._cardinal:
+			return f'{self.cardinal}'
+		else:
+			return f'{self._string()}'
+
+	@property
+	def cardinal(self):
+		dirs = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
+		ix = round(self / (360. / len(dirs)))
+		return dirs[ix % len(dirs)]
+
+
 @PropertiesFromConfig
 @NamedType
 class Voltage(Measurement):
@@ -9,24 +36,7 @@ class Voltage(Measurement):
 
 
 @NamedType
-class Direction(Measurement):
-	_precision = 0
-	_decorator = 'º'
-
-	def __init__(self, *args, **kwargs):
-		super(Direction, self).__init__(*args, **kwargs)
-
-	def __str__(self) -> str:
-		string = string = self._string()
-		return '{cardinal} ({value}{decorator})'.format(cardinal=self.cardinal, value=string, decorator=self._decorator)
-
-	@property
-	def cardinal(self):
-		dirs = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
-		ix = round(self / (360. / len(dirs)))
-		return dirs[ix % len(dirs)]
-
-@NamedType
+@PropertiesFromConfig
 class Strikes(Measurement):
 	# TODO: Create discrete number class for this and similar measurements
 	unit = 'strikes'
