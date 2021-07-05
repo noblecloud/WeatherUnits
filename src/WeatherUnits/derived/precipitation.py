8ef5241a5d630bddd9a8321b10c5b6d56941d705
Bryan Neal Garrison <git@noblecloud.io>
2021-07-05 14:24:06 -0400

    - Precipitation is now a hacky extension of Length - PrecipitationRate inherits both Precipitation and DistanceOverTime - PrecipitationRate three child classes have implied units and lengths of time requiring only one measurement for initiation.  Any numerical value or Time unit can be provided and it will be converted to the appropriate unit

--- src/WeatherUnits/derived/precipitation.py
+++ src/WeatherUnits/derived/precipitation.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from ..base import Measurement
 from .rate import DistanceOverTime
 from ..base.Decorators import NamedType, NamedSubType
 from ..length import Length
@@ -9,12 +10,7 @@ __all__ = ['Precipitation']
 
 
 @NamedType
-class Precipitation(DistanceOverTime):
-	_numerator: Length
-	_denominator: Time
-	Daily: type
-	Hourly: type
-	Minutely: type
+class Precipitation(Measurement):
 
 	class Type(Enum):
 		NONE = 0
@@ -23,71 +19,86 @@ class Precipitation(DistanceOverTime):
 		Sleet = 3
 		Snow = 4
 
+	def __new__(cls, value: Length, *args, **kwargs):
+		value = value.__class__(value, *args, **kwargs)
+		value.__dict__.update(Precipitation.__dict__)
+		return value
+
+
+@NamedType
+class PrecipitationRate(DistanceOverTime, Precipitation):
+	_numerator: Length
+	_denominator: Time
+	Daily: type
+	Hourly: type
+	Minutely: type
+
+	def __new__(cls, numerator, denominator: int = 1, *args, **kwargs):
+		value = float(numerator) / float(denominator)
+		return DistanceOverTime.__new__(cls, numerator, denominator, *args, **kwargs)
+
+	def __init__(self, numerator: Length, denominator: int = 1, *args, **kwargs):
+		if isinstance(denominator, int):
+			denominator = Time.Hour(denominator)
+		DistanceOverTime.__init__(self, numerator, denominator, *args, **kwargs)
+
 	@property
 	def fth(self):
-		return Precipitation(self._numerator.ft, self._denominator.hr)
+		return PrecipitationRate(self._numerator.foot, self._denominator.hour)
 
 	@property
 	def inh(self):
-		return Precipitation(self._numerator.inch, self._denominator.hr)
+		return PrecipitationRate(self._numerator.inch, self._denominator.hour)
 
 	@property
 	def ins(self):
-		return Precipitation(self._numerator.inch, self._denominator.s)
+		return PrecipitationRate(self._numerator.inch, self._denominator.second)
 
 	@property
 	def mmh(self):
-		return Precipitation(self._numerator.mm, self._denominator.hr)
+		return PrecipitationRate(self._numerator.millimeter, self._denominator.hour)
 
 	@property
 	def mms(self):
-		return Precipitation(self._numerator.mm, self._denominator.s)
+		return PrecipitationRate(self._numerator.millimeter, self._denominator.second)
 
 	@property
 	def cmh(self):
-		return Precipitation(self._numerator.cm, self._denominator.hr)
+		return PrecipitationRate(self._numerator.centimeter, self._denominator.hour)
 
 	@property
 	def mh(self):
-		return Precipitation(self._numerator.m, self._denominator.hr)
+		return PrecipitationRate(self._numerator.meter, self._denominator.hour)
 
 
 @NamedSubType
-class Daily(Precipitation):
-	# def __new__(cls, numerator: Length, denominator: int = 1):
-	# 	value = float(numerator) / float(denominator)
-	# 	return Measurement.__new__(cls, value)
+class Daily(PrecipitationRate):
 
-	def __init__(self, numerator: Length, denominator: int = 1):
-		self._numerator = numerator
-		self._denominator = Time.Day(denominator)
-		Measurement.__init__(self, float(self._numerator) / float(self._denominator))
+	def __init__(self, numerator: Length, denominator: int = 1, *args, **kwargs):
+		if isinstance(denominator, int):
+			denominator = Time.Day(denominator)
+		PrecipitationRate.__init__(self, numerator, denominator, *args, **kwargs)
 
 
 @NamedSubType
-class Hourly(Precipitation):
-	def __new__(cls, numerator: Length, denominator: int = 1):
-		value = float(numerator) / float(denominator)
-		return Measurement.__new__(cls, value)
+class Hourly(PrecipitationRate):
 
-	def __init__(self, numerator: Length, denominator: int = 1):
-		self._numerator = numerator
-		self._denominator = Time.Hour(denominator)
-		Measurement.__init__(self, float(self._numerator) / float(self._denominator))
+	def __init__(self, numerator: Length, denominator: int = 1, *args, **kwargs):
+		if isinstance(denominator, int):
+			denominator = Time.Hour(denominator)
+		PrecipitationRate.__init__(self, numerator, denominator, *args, **kwargs)
 
 
 @NamedSubType
-class Minutely(Precipitation):
-	def __new__(cls, numerator: Length, denominator: int = 1):
-		value = float(numerator) / float(denominator)
-		return Measurement.__new__(cls, value)
+class Minutely(PrecipitationRate):
 
-	def __init__(self, numerator: Length, denominator: int = 1):
-		self._numerator = numerator
-		self._denominator = Time.Minute(denominator)
-		Measurement.__init__(self, float(self._numerator) / float(self._denominator))
+	def __init__(self, numerator: Length, denominator: int = 1, *args, **kwargs):
+		if isinstance(denominator, int):
+			denominator = Time.Minute(denominator)
+		PrecipitationRate.__init__(self, numerator, denominator, *args, **kwargs)
 
 
+Precipitation.Rate = PrecipitationRate
 Precipitation.Daily = Daily
 Precipitation.Hourly = Hourly
 Precipitation.Minutely = Minutely
