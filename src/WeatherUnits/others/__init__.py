d3c05901f4cbb6b5b160e959f1074eebcd6d3a2d
Bryan Neal Garrison <git@noblecloud.io>
2021-10-11 18:22:15 -0400

    - Added Percentage class which the old Humidity inherits - Added Probability class - Added 'shorten' and 'max >= 2' to display only the two letter cardinal directions ['NW' vs 'NNW']

--- src/WeatherUnits/others/__init__.py
+++ src/WeatherUnits/others/__init__.py
@@ -1,27 +1,80 @@
+from fractions import Fraction
+from typing import Optional
+
 from ..base import NamedType
 from ..base import Measurement
 from . import light
 
 Light = light.Light
 
-__all__ = ['Light', 'Direction', 'Humidity', 'Voltage', 'Strikes']
+__all__ = ['Light', 'Angle', 'Direction', 'Humidity', 'Voltage', 'Strikes', 'Probability', 'Coverage', 'Percentage']
+
+
+class Percentage(Measurement):
+	_unit = ''
+	_decorator = '%'
+	_maxValue = 1.0
+	_minValue = 0.0
+
+	def __new__(cls, value, isPercentage: bool = None, limits: bool = True, *args, **kwargs):
+		if isPercentage is None:
+			if value > cls._maxValue:
+				value /= 100
+		elif isPercentage:
+			value /= 100
+
+		if limits:
+			value = min(cls._maxValue, max(value, cls._minValue))
+
+		return super(Percentage, cls).__new__(cls, value, *args, **kwargs)
+	
+	def _string(self, *args, **kwargs):
+		kwargs['multiplier'] = 100
+		return super(Percentage, self)._string(*args, **kwargs)
+
+	def __int__(self) -> int:
+		return int(float(self) * 100)
+
+	def fromFloat(self, value: float) -> 'Percentage':
+		return Percentage(value, isPercentage=False, limits=False)
 
 
 @NamedType
-class Humidity(Measurement):
-	_unit = ''
+class Humidity(Percentage):
 	_decorator = '%'
 
 
 @NamedType
-class Direction(Measurement):
+class Probability(Measurement):
+	_decorator = '%'
+	_fraction: Optional[Fraction]
+	_denominatorLimit: int = 10
+
+	def __init__(self, *args, **kwargs):
+		self._fraction: Optional[Fraction] = None
+		super(Probability, self).__init__(*args, **kwargs)
+
+	@property
+	def fraction(self) -> Fraction:
+		if self._fraction is None:
+			self._fraction = Fraction(self).limit_denominator(self._denominatorLimit)
+		return self._fraction
+
+
+@NamedType
+class Coverage(Percentage):
+	_decorator = '%'
+
+
+@NamedType
+class Angle(Measurement):
 	_precision = 0
 	_decorator = 'º'
 	_cardinal = True
-	_shorten = True
+	_max = 3
 
 	_dirsAbbrv = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
-	_dirsFull = ['North', 'North Northeast', 'East Northeast', 'East', 'East Southeast', 'Southeast',
+	_dirsFull = ['North', 'North Northeast', 'Northeast', 'East Northeast', 'East', 'East Southeast', 'Southeast',
 	             'South Southeast', 'South', 'South Southwest', 'Southwest', 'West Southwest', 'West',
 	             'West Northwest', 'Northwest', 'North Northwest']
 
@@ -35,7 +88,16 @@ class Direction(Measurement):
 
 	@property
 	def cardinal(self):
-		return self._dirsAbbrv[round(self / 22.5) % 16] if self._shorten else self._dirsFull[round(self / 22.5) % 16]
+		if self._max < 3 and self._shorten:
+			return self._dirsAbbrv[round(self / 45) % 8 * 2]
+		if self._shorten:
+			return self._dirsAbbrv[round(self / 22.5) % 16]
+		else:
+			return self._dirsFull[round(self / 22.5) % 16]
+
+	@property
+	def angle(self):
+		return Angle(self)
 
 	@property
 	def decorator(self):
