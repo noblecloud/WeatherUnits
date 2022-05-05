3e53972ede75337d97b134d95b29e6d9bba7dc19
Bryan Neal Garrison <git@noblecloud.io>
2022-05-05 15:55:46 -0400

    - Renamed PySide main window classes - Added automatic location lookup with IP - Cleaned up example-config
    Signed-off-by: Bryan Neal Garrison <git@noblecloud.io>

--- tests/test_length.py
+++ tests/test_length.py
@@ -20,10 +20,41 @@ class TestLength(TestCase):
 		self.assertEqual((self.inch * 12).foot, self.foot)
 
 	def test_foot(self):
-		self.assertEqual(self.foot * 3, self.yard.foot)
+		self.assertEqual(self.foot*3, self.yard.foot)
 
 	def test_yard(self):
-		self.assertEqual(self.yard, self.foot * 3)
+		self.assertEqual(self.yard, self.foot*3)
 
 	def test_mile(self):
 		self.assertEqual(length.Foot(5280).mile, self.mile)
+
+	def test_millimeter(self):
+		self.assertEqual(self.millimeter*1000, self.meter)
+
+	def test_centimeter(self):
+		self.assertEqual(self.centimeter*100, self.meter)
+
+	def test_meter(self):
+		self.assertEqual(self.meter*1000, self.kilometer)
+
+	def test_kilometer(self):
+		self.assertEqual(self.kilometer*1000, self.meter)
+
+	def test_conversion(self):
+		self.assertEqual(self.inch.centimeter, self.centimeter)
+
+	def test_invalid_conversion(self):
+		with self.assertRaises(ValueError):
+			self.inch.millimeter
+
+	def test_invalid_conversion_2(self):
+		with self.assertRaises(ValueError):
+			self.inch.kilometer
+
+	def test_invalid_conversion_3(self):
+		with self.assertRaises(ValueError):
+			self.centimeter.inch
+
+	def test_invalid_conversion_4(self):
+		with self.assertRaises(ValueError):
+			self.kilometer.mile
